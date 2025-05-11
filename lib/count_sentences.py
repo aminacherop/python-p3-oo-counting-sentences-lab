265ed552759dcb708a135328083daab796a2cb8f
aminacherop <cheropamina26@gmail.com>
2025-05-11 20:06:45 +0300

    counting sentence lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+  def __init__(self,value= ' '):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value (self,new_value):
+    if isinstance(new_value,str):
+      self._value = new_value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+  
+  def is_question(self):
+    return self.value.endswith('?')
+  
+  def is_exclamation(self):
+    return self.value.endswith('!')
+  
+  def count_sentences(self):
+    sentences = re.split(r'[.!?]+', self.value)
+    sentences = [s for s in sentences if s.strip()]
+    return len(sentences)
+  
+s = MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+print(s.count_sentences()) 
+  
+
+
+
+
+
+  #pass
